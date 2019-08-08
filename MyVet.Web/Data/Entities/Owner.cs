6b2c128b5ae8f8c821423adf382143954faeb23b
sebas <joanguisao187693@correo.itm.edu.co>
2019-08-07 20:06:59 -0500

    Create DB,SeedDB and Controllers

---
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Document")]
+        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Document { get; set; }
+
+        [Display(Name = "First Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Fixed Phone")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string FixedPhone { get; set; }
+
+        [Display(Name = "Cell Phone")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string CellPhone { get; set; }
+
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Address { get; set; }
+
+        [Display(Name = "Owner")]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Display(Name = "Owner")]
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
+        public ICollection<Pet> Pets { get; set; }
+
+        public ICollection<Agenda> Agenda { get; set; }
+
+    }
+}
+
